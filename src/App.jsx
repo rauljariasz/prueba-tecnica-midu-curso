050a251186a9ee89767fa47ee859c7701a8936da
raulariias <raularias24685@gmail.com>
2023-05-07 15:59:54 -0400

    retos principales listos

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,37 @@
+import { useState,  useEffect } from 'react'
 import './App.css'
+import { getImage, RANDOM_FACT } from './constants'
 
 function App() {
+  const [fact, setFact] = useState('')
+  const [imageUrl, setImageUrl] = useState(null)
+  
+
+  useEffect(() => {
+    fetch(RANDOM_FACT)
+    .then(res => res.json())
+    .then(data => {
+      setFact(data.fact)
+    })
+  }, [])
+
+  useEffect(() => {
+   if (!fact) {
+    return
+   } else {
+    const firstWord = fact.split(' ')[0]
+    setImageUrl(getImage(firstWord))
+   }
+  }, [fact])
 
   return (
-    <>
-      <h1>Hello World</h1>
-    </>
+    <main>
+      <h1>Cat App</h1>
+      <p>{fact}</p>
+      <img src={imageUrl} alt="Imagen random de un gato con la palabra" width={300} />
+    </main>
   )
 }
+
 
 export default App
---
+++ src/constants.js
@@ -0,0 +1,5 @@
+export const RANDOM_FACT = "https://catfact.ninja/fact"
+
+export const getImage = (fact) => {
+  return `https://cataas.com/cat/says/${fact}`
+}
